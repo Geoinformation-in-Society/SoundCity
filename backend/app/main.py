7bc2e13865443027f5e3b886caf5bfd0639fa588
Saidy Barry <barry@assutech.gm>
2026-01-30 22:52:33 +0100

    Add deployment config again

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://geoinformation-in-society.github.io",  # GitHub Pages
+        "https://geoinformation-in-society.github.io/SoundCity",  # GitHub Pages
         "https://web-production-1b810.up.railway.app",
     ],
     allow_credentials=True,
