1842507f0a887d87bd846bb3b4793d0ce7dbd308
Saidy Barry <barry@assutech.gm>
2026-01-30 23:44:06 +0100

    Add deployment config again

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,7 +8,6 @@ const apiClient = axios.create({
     'Content-Type': 'application/json',
     'Accept': 'application/json'
   },
-  timeout: 10000 // 10 second timeout
 })
 
 // Request interceptor for logging
